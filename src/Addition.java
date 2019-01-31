4c065bab22d54ba374f5f44605fd0ba1cdd38c96
Genovart <genovarm@emea.roche.com>
2019-01-31 14:54:11 +0100

    Inits repo

---
+++ src/Addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main (String[] args) { //psvm intro i surt tot automatic
+
+        System.out.println("Enter two numbers to make the sum: "); //sout
+
+        final Scanner scanner = new Scanner(System.in);
+
+        int a =scanner.nextInt();
+
+        int b =scanner.nextInt();
+
+        System.out.println("The sum is: " + sum(a, b));
+    }
+
+    private static int sum(int a, int b){
+        return a+b;
+    }
+}
